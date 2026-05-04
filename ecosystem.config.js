9b083d97c29190d193f0cb1267bdf9117392a0e9
Harsh Patel <harsh.patel@grewon.com>
2026-05-04 10:38:39 +0530

    minor change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'sx-mqtt-ingestor',
       script: './src/ingestor/index.js',
-      instances: 'max',
-      exec_mode: 'cluster',
+      instances: 1,
+      exec_mode: 'fork',
       env: {
         NODE_ENV: 'development',
       },
