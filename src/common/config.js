790faf7091f17dd6560baceaa8284c98c37388fe
Harsh Patel <harsh.patel@grewon.com>
2026-07-02 12:28:11 +0530

    minor change

--- src/common/config.js
+++ src/common/config.js
@@ -16,7 +16,7 @@ module.exports = {
   mqtt: {
     brokerUrl: process.env.MQTT_BROKER_URL,
     topic: process.env.MQTT_TOPIC || 'machines/+/+/telemetry',
-    testTopic: process.env.MQTT_TEST_TOPIC || 'test/#',
+    testTopic: process.env.MQTT_TEST_TOPIC || 'tests/#',
     clientId: process.env.MQTT_CLIENT_ID || 'sx-ingestor-service',
   },
   service: {
